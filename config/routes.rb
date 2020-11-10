c558616694172b2e8a58e89d1dddf311ab7f9ab9
Pierre Penhard <p.penhard@gmail.com>
2020-11-10 15:16:24 +0100

    Added exercise Rails Task Manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'tasks', to: 'tasks#index'
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/new', to: 'tasks#new'
+  # get 'task/:id', to: 'tasks#show', as: :task
+  # patch 'task/:id', to: 'tasks#update'
+  # delete 'task/:id', to: 'tasks#destroy'
+  # get 'task/:id/edit', to: 'tasks#edit', as: :edit_task
+
+  resources :tasks
 end
